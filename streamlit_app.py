642d931f86ce3655c6bd80269077525ceeda5ce5
Zainab <69767144+zoziqm@users.noreply.github.com>
2024-04-30 10:38:53 +0300

    Update streamlit_app.py
    see the data in a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,5 @@ if ingrediants_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fav_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
