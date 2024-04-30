9f44eaec52b74ea04745fc43448fe38f8f3debeb
Zainab <69767144+zoziqm@users.noreply.github.com>
2024-04-30 12:10:00 +0300

    Update streamlit_app.py
    correct typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.write('The name of your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),(col('SEARCH_ON')))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
